ef7039a8fdc8bb3572d2dd957467b1a95d49946d
Mhayk Whandson <hi@mhayk.com>
2020-03-09 10:00:37 +0000

    The controller named "UserController" has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,15 @@
+export default {
+  async store(req, res) {
+    const { name, email, password } = req.body;
+
+    const user = {
+      name,
+      email,
+      password,
+    };
+
+    // Send an email
+
+    return res.json(user);
+  },
+};
